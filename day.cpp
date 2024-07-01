0ea85e64ce0f3f07ff9a2af65c368bab8dc1015e
ravinduHV <ravinduhvimarshana@gmail.com>
2024-07-01 22:55:36 +0530

    first commits

---
+++ day.cpp
@@ -0,0 +1,25 @@
+#include "day.h"
+
+void day::show_events()
+{
+}
+
+void day::add_event(event _event)
+{
+}
+
+void day::remove_event(int id)
+{
+}
+
+void day::edit_event(int id)
+{
+}
+
+void day::shift_event(int id, string starting_time, string ending_time)
+{
+}
+
+void day::show_meetings()
+{
+}
