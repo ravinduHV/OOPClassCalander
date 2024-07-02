f113a670d1e63677e6b83b4ddd5e76694bc44557
ravinduHV <ravinduhvimarshana@gmail.com>
2024-07-02 23:28:00 +0530

    menu 1 created

--- main.cpp
+++ main.cpp
@@ -8,7 +8,30 @@
 
 using namespace std;
 
+int menu_1();
+
 int main()
 {
+    menu_1();
     
+}
+
+
+int menu_1(){
+    int choice;
+    cout << "View Schedule\nAdd New meeting\nShow details of individual meetings\nAdd day offs\nMeeting Controls\n";
+    
+    while(true)
+    {
+        cin >> choice;
+        if (choice < 1 || choice > 5)
+        {
+            cout << "Invalid choice, please try again :";
+        }
+        else
+        {
+            return choice;
+        }
+    }
+
 }
